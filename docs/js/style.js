2839fe54b6446c6989b8fc81958804b74c7ce6b9
Grace Qiu <gracezqiu@gmail.com>
2022-05-09 19:00:04 -0400

    Make primary nav item stay selected when secondary nav open

--- docs/js/style.js
+++ docs/js/style.js
@@ -3,5 +3,11 @@ $(document).ready(function() {
 });
 
 $("#primaryNavProjects").click(function() {
+    if ($("#secondaryNav").is(":hidden")) {
+        $(this).find("#secondaryNavToggle").css({"text-decoration": "underline", "color": "#32584C"});
+    } else {
+        $(this).find("#secondaryNavToggle").css({"text-decoration": "none", "color": "#181717"});
+    }
     $("#secondaryNav").slideToggle();
 });
+
